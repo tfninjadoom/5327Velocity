7c4a044ad23630d709154b502ee42d3a198ca6ed
AgilanSam <138312709+AgilanSam@users.noreply.github.com>
2024-06-08 15:56:31 -0700

    first

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       sammy                                                     */
+/*    Created:      6/8/2024, 3:02:41 PM                                      */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+
+    Brain.Screen.printAt( 10, 50, "Hello V5" );
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
